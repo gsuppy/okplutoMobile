30bc9b426986dd7599ff0fa4cd5b87a7fdaead35
PHDorin <hanshirosan@gmail.com>
2016-11-04 13:01:59 -0600

    Added Profile Display basic component without props

---
+++ OKPlutoMobile/src/components/profile.js
@@ -0,0 +1,73 @@
+'use strict';
+import React, {Component} from 'react';
+import {View, ScrollView, Text, Image, Linking} from 'react-native';
+
+
+// <Image source={this.props.userInfo.profilepic} />
+// {this.props.userInfo.firstname} {this.props.userInfo.lastname}
+// {this.props.userInfo.loc}
+// <Image source={this.props.userInfo.picLink} alt="Pic"/>
+// {this.props.userInfo.dogname}
+// {this.props.userInfo.dogBreed}
+// {this.props.userInfo.dogAge}
+// <ProfileEditDialog userInfo={this.props.userInfo} resetUserInfo={this.props.resetUserInfo} />
+class ProfileDisplay extends Component {
+  constructor(props) {
+  super(props);
+  }
+
+
+  render(){
+    return (
+      <ScrollView>
+
+        <View>
+          <Text> 
+            This is where the profilepic will go 
+          </Text>
+        </View>
+
+        <View>
+
+          <View>
+            <Text> 
+              Your Info 
+            </Text>
+          </View>
+
+          <Text>Name: Have to get user name from props</Text>
+
+          <Text>Location: Have to get location from props</Text>
+  
+          
+          <View>
+            <Text>
+              This will be a pic of You
+            </Text>
+          </View> 
+          
+      
+
+          <View>
+            <Text> 
+              Your Pup's Info
+            </Text>
+          </View>
+
+            <Text>Name: </Text>
+            <Text>Breed: </Text>
+            <Text>Age: </Text>
+
+          <Text>
+            We Need To add an Edit User Profile Button
+          </Text>
+
+        </View>
+
+      </ScrollView>
+    )
+  }   
+}
+
+
+module.exports = ProfileDisplay;
